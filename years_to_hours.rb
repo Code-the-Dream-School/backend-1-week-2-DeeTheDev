6263f077a65824a9e3e9c916e0524f583c52dc87
Denilson V <deethedev@gmail.com>
2020-10-07 23:16:56 -0400

    Added rb file with first function

---
+++ years_to_hours.rb
@@ -0,0 +1,9 @@
+def years_to_hours
+    puts "Enter a number of years" 
+    years = gets.chomp # this returns a string 
+    years = years.to_i # this converts a string to an integer 
+    hours = years * 365 * 24; 
+    puts "That's #{hours} hours."
+end
+
+years_to_hours
