cf5c027d4661149a76a815d88c44a163c4547765
Denilson V <deethedev@gmail.com>
2020-10-17 10:59:07 -0400

    Completed Backend 1 Lesson 2 Assignment

--- years_to_hours.rb
+++ years_to_hours.rb
@@ -6,4 +6,24 @@ def years_to_hours
     puts "That's #{hours} hours."
 end
 
+def decades_to_minutes
+    puts "Enter a number of decades"
+    decades = gets.chomp
+    decades= decades.to_i
+    hours_in_decades = 87600
+    minutes = decades * hours_in_decades * 60
+    puts "That's #{minutes} minutes."
+end
+
+def years_to_seconds
+    puts "Enter your age"
+    years = gets.chomp
+    years = years.to_i
+    minutes_in_year = 525600
+    seconds = years * minutes_in_year * 60
+    puts "That's #{seconds} seconds!"
+end
+
 years_to_hours
+decades_to_minutes
+years_to_seconds
